83cf1ed1fd28c94d5f95c23e13525488a90c7482
dmi3kno <dperepolkin@gmail.com>
2018-07-21 14:45:42 +0200

    initial commit

---
+++ R/crawl.R
@@ -0,0 +1,10 @@
+#' Crawl www.cheese.com and return tidy dataframe with information about world cheeses
+#'
+#' @param path address of cheese.com website
+#'
+#' @return
+#'
+#' @examples
+crawl_cheese_com <- function(path="www.cheese.com"){
+  TRUE
+}
